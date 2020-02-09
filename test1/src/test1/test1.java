8e0561e2edc7885a07037d88dce21fcef68f6ac5
asus <asus@DESKTOP-NB1D6HH>
2020-02-09 12:52:34 +0800

    最新

--- test1/src/test1/test1.java
+++ test1/src/test1/test1.java
@@ -1,7 +1,8 @@
 package test1;
 
 public class test1 {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		System.out.println(1);
+		System.out.println(2);
 	}
 }
